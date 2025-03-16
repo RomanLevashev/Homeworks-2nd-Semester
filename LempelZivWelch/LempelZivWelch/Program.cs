1bd18f5094c349d3bcf52d5d3565b8e955f894cd
RomanLevashev <rolevashev@gmail.com>
2025-03-16 14:49:09 +0300

    Tests for ByteTrie and LZW added

--- LempelZivWelch/LempelZivWelch/Program.cs
+++ LempelZivWelch/LempelZivWelch/Program.cs
@@ -1,2 +1,5 @@
 ﻿using System.Collections;
 using LempelZivWelch;
+
+LZW.CompressFile("C:\\Users\\Shinsei\\Downloads\\lzw_test_file.txt");
+LZW.DecompressFile("C:\\Users\\Shinsei\\Downloads\\lzw_test_file.txt.zipped");
---
+++ LempelZivWelch/UnitTests/TrieTests.cs
@@ -0,0 +1,29 @@
+﻿namespace UnitTests
+{
+    using ByteTrie;
+
+    /// <summary>
+    /// Contains unit tests for the Trie class.
+    /// </summary>
+    [TestClass]
+    public sealed class TrieTests
+    {
+        /// <summary>
+        /// Tests the <see cref="Trie.Add(byte[])"/> and <see cref="Trie.Contains(byte[])"/> methods.
+        /// Verifies that elements can be successfully added to the Trie and then checked for existence.
+        /// </summary>
+        [TestMethod]
+        public void AddAndContainsTest()
+        {
+            Trie trie = new();
+            trie.Add((byte)0);
+            trie.Add(new byte[] { 0, 1 });
+            trie.Add(new byte[] { 0, 1, 2 });
+
+            Assert.IsFalse(trie.Add((byte)0).isSuccess);
+            Assert.IsTrue(trie.Contains((byte)0));
+            Assert.IsTrue(trie.Contains(new byte[] { 0, 1, 2 }));
+            Assert.IsTrue(trie.Contains(new byte[] { 0, 1 }));
+        }
+    }
+}
