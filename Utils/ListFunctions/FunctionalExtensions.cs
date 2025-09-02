dd8447f32aeaf9042c7d017d22db6ca9e15f26d4
Roman <rolevashev@gmail.com>
2025-09-02 22:25:00 +0300

    - Add license headers to all source files - Include stylecop.json configuration - Add tests for string lists and empty lists scenarios

--- Utils/ListFunctions/FunctionalExtensions.cs
+++ Utils/ListFunctions/FunctionalExtensions.cs
@@ -1,71 +1,75 @@
-﻿namespace ListFunctions
+﻿// <copyright file="FunctionalExtensions.cs" company="Roman Levashev">
+// Copyright (c) Roman Levashev. All rights reserved.
+// Licensed under the MIT License.
+// </copyright>
+
+namespace ListFunctions;
+
+/// <summary>
+/// Provides functional extensions for IEnumerable.
+/// </summary>
+public static class FunctionalExtensions
 {
     /// <summary>
-    /// Provides functional extensions for IEnumerable.
+    /// Transforms each element of a sequence into a new form (Projection operation).
     /// </summary>
-    public static class FunctionalExtensions
+    /// <typeparam name="T">The type of the elements in the source sequence.</typeparam>
+    /// <typeparam name="TResult">The type of the elements in the result sequence.</typeparam>
+    /// <param name="source">The source sequence to transform.</param>
+    /// <param name="func">A transform function to apply to each element.</param>
+    /// <returns>A List whose elements are the result of invoking the transform function on each element of source.</returns>
+    public static List<TResult> Map<T, TResult>(this IEnumerable<T> source, Func<T, TResult> func)
     {
-        /// <summary>
-        /// Transforms each element of a sequence into a new form (Projection operation).
-        /// </summary>
-        /// <typeparam name="T">The type of the elements in the source sequence.</typeparam>
-        /// <typeparam name="TResult">The type of the elements in the result sequence.</typeparam>
-        /// <param name="source">The source sequence to transform.</param>
-        /// <param name="func">A transform function to apply to each element.</param>
-        /// <returns>A List whose elements are the result of invoking the transform function on each element of source.</returns>
-        public static List<TResult> Map<T, TResult>(this IEnumerable<T> source, Func<T, TResult> func)
+        List<TResult> result = [];
+
+        foreach (var item in source)
         {
-            List<TResult> result = [];
-
-            foreach (var item in source)
-            {
-                result.Add(func(item));
-            }
-
-            return result;
+            result.Add(func(item));
         }
 
-        /// <summary>
-        /// Filters a sequence of values based on a predicate.
-        /// </summary>
-        /// <typeparam name="T">The type of the elements in the source sequence.</typeparam>
-        /// <param name="source">The source sequence to filter.</param>
-        /// <param name="filter">A function to test each element for a condition.</param>
-        /// <returns>A List that contains elements from the input sequence that satisfy the condition.</returns>
-        public static List<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> filter)
+        return result;
+    }
+
+    /// <summary>
+    /// Filters a sequence of values based on a predicate.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the source sequence.</typeparam>
+    /// <param name="source">The source sequence to filter.</param>
+    /// <param name="filter">A function to test each element for a condition.</param>
+    /// <returns>A List that contains elements from the input sequence that satisfy the condition.</returns>
+    public static List<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> filter)
+    {
+        List<T> result = [];
+
+        foreach (var item in source)
         {
-            List<T> result = [];
-
-            foreach (var item in source)
+            if (filter(item))
             {
-                if (filter(item))
-                {
-                    result.Add(item);
-                }
+                result.Add(item);
             }
-
-            return result;
         }
 
-        /// <summary>
-        /// Performs a reduction operation on a sequence (also known as aggregate/fold operation).
-        /// </summary>
-        /// <typeparam name="T">The type of the elements in the source sequence.</typeparam>
-        /// <typeparam name="TAccumulate">The type of the accumulator value.</typeparam>
-        /// <param name="source">The source sequence to reduce.</param>
-        /// <param name="seed">The initial accumulator value.</param>
-        /// <param name="func">An accumulator function to be invoked on each element.</param>
-        /// <returns>The final accumulator value.</returns>
-        public static TAccumulate Fold<T, TAccumulate>(this IEnumerable<T> source, TAccumulate seed, Func<TAccumulate, T, TAccumulate> func)
+        return result;
+    }
+
+    /// <summary>
+    /// Performs a reduction operation on a sequence (also known as aggregate/fold operation).
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the source sequence.</typeparam>
+    /// <typeparam name="TAccumulate">The type of the accumulator value.</typeparam>
+    /// <param name="source">The source sequence to reduce.</param>
+    /// <param name="seed">The initial accumulator value.</param>
+    /// <param name="func">An accumulator function to be invoked on each element.</param>
+    /// <returns>The final accumulator value.</returns>
+    public static TAccumulate Fold<T, TAccumulate>(this IEnumerable<T> source, TAccumulate seed, Func<TAccumulate, T, TAccumulate> func)
+    {
+        TAccumulate accumulator = seed;
+
+        foreach (var item in source)
         {
-            TAccumulate accumulator = seed;
-
-            foreach (var item in source)
-            {
-                accumulator = func(accumulator, item);
-            }
-
-            return accumulator;
+            accumulator = func(accumulator, item);
         }
+
+        return accumulator;
     }
 }
--- Utils/ListFunctionsTest/FunctionalExtensionsTest.cs
+++ Utils/ListFunctionsTest/FunctionalExtensionsTest.cs
@@ -1,50 +1,132 @@
-﻿namespace ListFunctionsTest
+﻿// <copyright file="FunctionalExtensionsTest.cs" company="Roman Levashev">
+// Copyright (c) Roman Levashev. All rights reserved.
+// Licensed under the MIT License.
+// </copyright>
+
+namespace ListFunctionsTest;
+
+using ListFunctions;
+
+/// <summary>
+/// Contains unit tests for the FunctionalExtensions class.
+/// </summary>
+[TestClass]
+public sealed class FunctionalExtensionsTest
 {
-    using ListFunctions;
+    /// <summary>
+    /// Tests the Map extension method by applying a transformation function to each element.
+    /// </summary>
+    [TestMethod]
+    public void MapWithIntTest()
+    {
+        Func<int, int> plus = (x) => x + 2;
+        List<int> testList = [1, 2, 3];
+        List<int> expectedList = [3, 4, 5];
+
+        Assert.IsTrue(expectedList.SequenceEqual(testList.Map(plus)));
+    }
 
     /// <summary>
-    /// Contains unit tests for the FunctionalExtensions class.
+    /// Tests the Map extension method with string transformation.
     /// </summary>
-    [TestClass]
-    public sealed class FunctionalExtensionsTest
+    [TestMethod]
+    public void MapWithStringTest()
     {
-        /// <summary>
-        /// Tests the Map extension method by applying a transformation function to each element.
-        /// </summary>
-        [TestMethod]
-        public void MapTest()
-        {
-            Func<int, int> plus = (x) => x + 2;
-            List<int> testList = [1, 2, 3];
-            List<int> expectedList = [3, 4, 5];
+        Func<string, string> toUpper = (s) => s.ToUpper();
+        List<string> testList = ["hello", "world", "test"];
+        List<string> expectedList = ["HELLO", "WORLD", "TEST"];
 
-            Assert.IsTrue(expectedList.SequenceEqual(testList.Map(plus)));
-        }
+        Assert.IsTrue(expectedList.SequenceEqual(testList.Map(toUpper)));
+    }
 
-        /// <summary>
-        /// Tests the Filter extension method by selecting elements that match a predicate.
-        /// </summary>
-        [TestMethod]
-        public void FilterTest()
-        {
-            Func<int, bool> isEven = (x) => x % 2 == 0;
-            List<int> testList = [1, 2, 3];
-            List<int> expectedList = [2];
+    /// <summary>
+    /// Tests the Map extension method with empty list.
+    /// </summary>
+    [TestMethod]
+    public void MapWithEmptyListTest()
+    {
+        Func<int, int> plus = (x) => x + 2;
+        List<int> testList = [];
+        List<int> expectedList = [];
 
-            Assert.IsTrue(expectedList.SequenceEqual(testList.Filter(isEven)));
-        }
+        Assert.IsTrue(expectedList.SequenceEqual(testList.Map(plus)));
+    }
 
-        /// <summary>
-        /// Tests the Fold extension method by accumulating values with a reduction function.
-        /// </summary>
-        [TestMethod]
-        public void FoldTest()
-        {
-            Func<int, int, int> multiply = (acc, elem) => acc * elem;
-            List<int> testList = [1, 2, 3];
-            int seed = 1;
+    /// <summary>
+    /// Tests the Filter extension method by selecting elements that match a predicate.
+    /// </summary>
+    [TestMethod]
+    public void FilterWithIntTest()
+    {
+        Func<int, bool> isEven = (x) => x % 2 == 0;
+        List<int> testList = [1, 2, 3];
+        List<int> expectedList = [2];
 
-            Assert.AreEqual(6, testList.Fold(seed, multiply));
-        }
+        Assert.IsTrue(expectedList.SequenceEqual(testList.Filter(isEven)));
+    }
+
+    /// <summary>
+    /// Tests the Filter extension method with string predicate.
+    /// </summary>
+    [TestMethod]
+    public void FilterWithStringTest()
+    {
+        Func<string, bool> startsWithH = (s) => s.StartsWith("h", StringComparison.OrdinalIgnoreCase);
+        List<string> testList = ["hello", "world", "Hi", "test"];
+        List<string> expectedList = ["hello", "Hi"];
+
+        Assert.IsTrue(expectedList.SequenceEqual(testList.Filter(startsWithH)));
+    }
+
+    /// <summary>
+    /// Tests the Filter extension method with empty list.
+    /// </summary>
+    [TestMethod]
+    public void FilterWithEmptyListTest()
+    {
+        Func<int, bool> isEven = (x) => x % 2 == 0;
+        List<int> testList = [];
+        List<int> expectedList = [];
+
+        Assert.IsTrue(expectedList.SequenceEqual(testList.Filter(isEven)));
+    }
+
+    /// <summary>
+    /// Tests the Fold extension method by accumulating values with a reduction function.
+    /// </summary>
+    [TestMethod]
+    public void FoldWithIntTest()
+    {
+        Func<int, int, int> multiply = (acc, elem) => acc * elem;
+        List<int> testList = [1, 2, 3];
+        int seed = 1;
+
+        Assert.AreEqual(6, testList.Fold(seed, multiply));
+    }
+
+    /// <summary>
+    /// Tests the Fold extension method with string concatenation.
+    /// </summary>
+    [TestMethod]
+    public void FoldWithStringTest()
+    {
+        Func<string, string, string> concat = (acc, elem) => acc + elem;
+        List<string> testList = ["a", "b", "c"];
+        string seed = string.Empty;
+
+        Assert.AreEqual("abc", testList.Fold(seed, concat));
+    }
+
+    /// <summary>
+    /// Tests the Fold extension method with empty list.
+    /// </summary>
+    [TestMethod]
+    public void FoldWithEmptyListTest()
+    {
+        Func<int, int, int> multiply = (acc, elem) => acc * elem;
+        List<int> testList = [];
+        int seed = 1;
+
+        Assert.AreEqual(seed, testList.Fold(seed, multiply));
     }
 }
